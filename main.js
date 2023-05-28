aa9cec96fa2adf120d9e9429226df8bb78503103
unknown <dat_muslimguy@icloud.com>
2023-05-28 16:03:49 -0500

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+const popupEl = document.getElementById("popup");
+
+function openPopup(){
+    popupEl.classList.add("open-popup");
+}
+function closePopup(){
+    popupEl.classList.remove("open-popup");
+}
